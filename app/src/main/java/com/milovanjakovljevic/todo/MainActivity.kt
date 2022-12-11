e8e16523f15d4c646d5ad81a90bc8487b6955e8a
Milovan99 <milovanjakovljevic78@gmail.com>
2022-12-11 19:59:12 +0100

    Brisanje nepotrepnog dela

--- app/src/main/java/com/milovanjakovljevic/todo/MainActivity.kt
+++ app/src/main/java/com/milovanjakovljevic/todo/MainActivity.kt
@@ -24,9 +24,7 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+
 
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -50,9 +48,5 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
+
 }
